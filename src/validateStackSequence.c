a7b4ae2346d56110ce6c4498490560886d98ca12
Dipro Soren <diprosoren15@gmail.com>
2023-04-13 23:36:20 +0530

    removed error

--- src/validateStackSequence.c
+++ src/validateStackSequence.c
@@ -10,12 +10,11 @@ struct stack
 };
 
 void push(struct stack* a, int val) {
-    if(a->top!=a->size-1) {
-        a->top++;
-        a->arr[a->top]=val;
-    }
-    return;
-    printf("Stack Overflow");
+    if(a->top > a->size-1) return;
+		 
+	
+	a->top++;
+    a->arr[a->top]=val;
 }
 int pop(struct stack* a)
 {
@@ -30,34 +29,30 @@ int pop(struct stack* a)
 
 int peek(struct stack* a)
 {
-	int arrayind=a->top;
-	if(arrayind!=0)
-		return a->arr[arrayind];
-
-		return 0;
+	return a->arr[a->top];
 }
 
 bool validateStackSequences(int* pushed, int pushedSize, int* popped, int poppedSize){
 
-	struct stack s;
-	s.size=pushedSize;
-	s.top=-1;
-	s.arr=(int*)malloc(s.size*sizeof(int));
+	struct stack* s =(struct stack*)malloc(sizeof(struct stack*));
+	s->size=pushedSize;
+	s->top=-1;
+	s->arr=(int*)malloc(s->size*sizeof(int));
 	int j=0;
 	for(int i=0;i<pushedSize;i++)
 	{
-	    s.top++;
-		s.arr[i]=pushed[i];
+	    push(s,pushed[i]);
         
-		while(s.size!=0 && peek(&s)==popped[j])
+		while(s->top!=-1 && peek(s)==popped[j])
 		{
-			pop(&s);
+			pop(s);
 			j++;
 		}
 	}
-	if(s.size==0)
+	if(s->top==-1)
 	return true;
 	else return false;
+	
 	
 }
 
@@ -68,5 +63,5 @@ int main()
 	int popped[]={4,5,3,2,1};
 	int poppedSize=pushedSize;
 
-	validateStackSequences(pushed, pushedSize, popped, poppedSize);
+	printf("%d\n",validateStackSequences(pushed, pushedSize, popped, poppedSize));
 }
